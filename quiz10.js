27f38b611f58b79445c68484b0a0d7158ddb5041
Jonathan Andrews <ionathas78@hotmail.com>
2020-04-08 08:53:10 -0500

    Changed quiz10 to use objects instead of array offsets

--- quiz10.js
+++ quiz10.js
@@ -7,23 +7,23 @@ function quiz10() {
         //  Question - Type - Answer
         //  'c' for a confirm (text) question, 'p' for a prompt (yes/no) question. 'a' to display an alert (Question won't be counted. Answer is disregarded).
         //  Answer should be in lowercase.
-        /* - */ "See if you can answer the following questions about the Dungeons & Dragons Roleplaying Game correctly:", "a", null,
+        /* - */ {question: "See if you can answer the following questions about the Dungeons & Dragons Roleplaying Game correctly:", type: "a", answer: null},
         
-        /* 1 */ "Faerun is the name of a major continent in the Forgotten Realms setting.", "c", true,
-        /* 2 */ "There have been 4 ability scores in the last three editions of D&D, grouped with half physical attributes and half mental or social attributes.", "c", false,
-        /* 3 */ "Corellon Latherian is the god of the elves.", "c", true,
-        /* 4 */ "Druids are primal champions connected to the spirits of the land they protect that wield divine spells and can transform into animals.", "c", true,
-        /* 5 */ "Since 2nd edition AD&D, the most common D&D alignment system has 2 axes: Good vs. Evil and Elves vs. Dwarves.", "c", false,
-        /* 6 */ "The Sword Coast is a busy region on the shores of the Sea of Fallen Stars. It's full of commerce and danger, and home to such cities as Waterdeep, Neverwinter, and Cormyr.", "c", false,
-        /* 7 */ "Drow are dark elves that live in a subterranean, matriarchal society and have the innate ability to cast certain magical spells.", "c", true,
-        /* 8 */ "A broadsword is any blade 9 inches or shorter that inflicts a d4 damage die for a human-sized wielder.", "c", false,
-        /* 9 */ "In 2nd edition D&D, only a human with a Charisma of 17 or 18 could be take the Paladin class.", "c", true,
-        /* 10 */ "A beholder is a terrifying aberration that has one enormous eye in the center of its round body, with ten eyestalks protruding from its head.", "c", true,
+        /* 1 */ {question: "Faerun is the name of a major continent in the Forgotten Realms setting.", type: "c", answer: true},
+        /* 2 */ {question: "There have been 4 ability scores in the last three editions of D&D, grouped with half physical attributes and half mental or social attributes.", type: "c", answer: false},
+        /* 3 */ {question: "Corellon Latherian is the god of the elves.", type: "c", answer: true},
+        /* 4 */ {question: "Druids are primal champions connected to the spirits of the land they protect that wield divine spells and can transform into animals.", type: "c", answer: true},
+        /* 5 */ {question: "Since 2nd edition AD&D, the most common D&D alignment system has 2 axes: Good vs. Evil and Elves vs. Dwarves.", type: "c", answer: false},
+        /* 6 */ {question: "The Sword Coast is a busy region on the shores of the Sea of Fallen Stars. It's full of commerce and danger, and home to such cities as Waterdeep, Neverwinter, and Cormyr.", type: "c", answer: false},
+        /* 7 */ {question: "Drow are dark elves that live in a subterranean, matriarchal society and have the innate ability to cast certain magical spells.", type: "c", answer: true},
+        /* 8 */ {question: "A broadsword is any blade 9 inches or shorter that inflicts a d4 damage die for a human-sized wielder.", type: "c", answer: false},
+        /* 9 */ {question: "In 2nd edition D&D, only a human with a Charisma of 17 or 18 could be take the Paladin class.", type: "c", answer: true},
+        /* 10 */ {question: "A beholder is a terrifying aberration that has one enormous eye in the center of its round body, with ten eyestalks protruding from its head.", type: "c", answer: true},
         
-        /* - */ "And a bonus question:", "a", null,
-        /* 11 */ "In roleplaying games, a Die with 20 sides is called what?", "p", "d20",
+        /* - */ {question: "And a bonus question:", type: "a", answer: null},
+        /* 11 */ {question: "In roleplaying games, a Die with 20 sides is called what?", type: "p", answer: "d20"},
         
-        /* - */ "Dungeons & Dragons, Forgotten Realms, and all associated names are the sole property of Wizards of the Coast.", "a", null
+        /* - */ {question: "Dungeons & Dragons, Forgotten Realms, and all associated names are the sole property of Wizards of the Coast.", type: "a", answer: null}
     ];
 
     //  Variable declaration
@@ -46,8 +46,8 @@ function quiz10() {
     var isQuestion = false;
 
     //  Shuffle Questions
-    for (var i = 0; i < quiz10Array.length; i = i + 3) {
-        currentType = quiz10Array[i + 1];
+    for (var i = 0; i < quiz10Array.length; i++) {
+        currentType = quiz10Array[i].type;
         currentType = tokenizeType(currentType);
         isQuestion = ((currentType == "c") || (currentType == "p"));
 
@@ -62,9 +62,9 @@ function quiz10() {
         arrayIndex = quiz10Shuffle[j];
 
         //  Set up current question.
-        currentPrompt = quiz10Array[arrayIndex];
-        currentType = quiz10Array[arrayIndex + 1];
-        currentAnswer = quiz10Array[arrayIndex + 2];
+        currentPrompt = quiz10Array[arrayIndex].question;
+        currentType = quiz10Array[arrayIndex].type;
+        currentAnswer = quiz10Array[arrayIndex].answer;
         
         currentType = tokenizeType(currentType);
 
